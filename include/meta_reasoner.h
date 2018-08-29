35d93a5124c0ea938599f490bdf3a2dc4efba166
zephirefaith <priyam8parashar@gmail.com>
2018-08-29 16:09:13 -0700

    skeleton

---
+++ include/meta_reasoner.h
@@ -0,0 +1,16 @@
+#ifndef META_REASONER_h
+#define META_REASONER_h
+
+class MetaReasoner {
+ private:
+  /* data */
+ public:
+  MetaReasoner(/* args */);
+  ~MetaReasoner();
+};
+
+MetaReasoner::MetaReasoner(/* args */) {}
+
+MetaReasoner::~MetaReasoner() {}
+
+#endif
