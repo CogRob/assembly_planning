3048870196b12020c97681b147e9c8bd6c24e075
zephirefaith <priyam8parashar@gmail.com>
2018-08-29 16:09:06 -0700

    +guards, +data org

--- include/lego_world.h
+++ include/lego_world.h
@@ -1,16 +1,32 @@
+#ifndef LEGO_WORLD_H
+#define LEGO_WORLD_H
+
+#include <vector>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Pose2D.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <ros/ros.h>
 #include <iostream>
 
-enum PrimitiveActions { DETECT, ALIGN, TRANSPORT, RETRACT, PICKUP, PLACE };
-enum ConstraintType { POSE, OBJECT, BOTH };
+enum ObjectOccupancy {INVENTORY, WORKSPACE };
+enum ObjectColor {}
 
 struct EnvState {
   /* data */
+  std::vector<ObjectDesc> object_list;
+  std::vector<ObjectState> object_state_list;
 };
 
 struct ObjectDesc {
-  /* data */
+  int length;
+  int width;
+  ObjectColor color;
 };
+
+struct ObjectState
+{
+  geometry_msgs::Pose2D pose;
+  ObjectOccupancy location;
+};
+
+#endif /* LEGO_WORLD_H */
