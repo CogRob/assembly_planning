7f74545c655bef8d64d54d42ef1a6bf900f61623
zephirefaith <priyam8parashar@gmail.com>
2018-08-29 18:22:55 -0700

    +initializations

--- include/lego_world.h
+++ include/lego_world.h
@@ -1,15 +1,15 @@
 #ifndef LEGO_WORLD_H
 #define LEGO_WORLD_H
 
-#include <vector>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Pose2D.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <ros/ros.h>
 #include <iostream>
+#include <vector>
 
-enum ObjectOccupancy {INVENTORY, WORKSPACE };
-enum ObjectColor {}
+enum ObjectOccupancy { INVENTORY, WORKSPACE, UNKNOWN };
+enum ObjectColor { RED, GREEN, BLUE, YELLOW, GREY };
 
 struct EnvState {
   /* data */
@@ -18,14 +18,19 @@ struct EnvState {
 };
 
 struct ObjectDesc {
+  ObjectDesc() : length(0), width(0), color(ObjectColor::GREY) {}
   int length;
   int width;
   ObjectColor color;
 };
 
-struct ObjectState
-{
+struct ObjectState {
+  ObjectState()
+      : pose(geometry_msgs::Pose2D()),
+        pose_set(false),
+        location(ObjectOccupancy::UNKNOWN) {}
   geometry_msgs::Pose2D pose;
+  bool pose_set;
   ObjectOccupancy location;
 };
 
