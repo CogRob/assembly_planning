d93ad882b7ba9d5d5ff32e8d5b58c009a02daeda
james <jameslathamsmith@gmail.com>
2018-09-15 14:35:33 -0700

    Changed Pose to Pose2D

--- src/metareasoning_agent/utilities.py
+++ src/metareasoning_agent/utilities.py
@@ -10,12 +10,13 @@ def calculate_pose_diff(pose1, pose2):
     Method to store the spatial distance between centroids of two blocks
 
     Input:
-        pose1: type - geometry_msgs/Pose
-        pose2: type - geometry_msgs/Pose
+        pose1: type - geometry_msgs/Pose2D
+        pose2: type - geometry_msgs/Pose2d
     Output:
         delta_pose: type - geometry_msgs/Point
     """
     pose_diff = Point()
-    pose_diff.x = pose1.position.x - pose2.position.x
-    pose_diff.y = pose1.position.y - pose2.position.y
+    pose_diff.x = pose1.x - pose2.x
+    pose_diff.y = pose1.y - pose2.y
+
     return pose_diff
