57ee16167306cea943ceaf79f81299c6b7913c31
james <jameslathamsmith@gmail.com>
2018-09-15 17:55:16 -0700

    Updated fault checking and added barbell symbol case

--- scripts/env_state_test.py
+++ scripts/env_state_test.py
@@ -3,11 +3,17 @@ from metareasoning_agent import agent
 from metareasoning_agent import knowledge_base as kb
 
 import rospy
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import Point, Pose2D
+
+import networkx.algorithms.isomorphism as iso
+import networkx as nx
+
+import math
 
 
 def generate_pick_and_place(block, location, pick_orientation, place_orientation):
-    resting_position = resting_position = Point(x=0.75, y=-.65, z=0.0)
+    resting_position = Point(x=0.75, y=-.65, z=0.0)
+
     return [
         {'action': kb.PrimitiveActions.detect,
             'constraints': agent.Constraints(),
@@ -59,7 +65,7 @@ def generate_pick_and_place(block, location, pick_orientation, place_orientation
 
         {'action': kb.PrimitiveActions.retract,
             'constraints': agent.Constraints(),
-            'desc': "Retracting..."
+            'desc': "Retracting...",
          },
 
         {'action': kb.PrimitiveActions.transport,
@@ -68,7 +74,6 @@ def generate_pick_and_place(block, location, pick_orientation, place_orientation
                                              orientation=0),
             'desc': "Moving back to starting pose",
          },
-        None,
     ]
 
 
@@ -80,6 +85,11 @@ def main():
     test_agent = agent.Agent('right')  # pylint: disable=C0103
     test_agent.subscribe()
 
+    #    B
+    # _ BB
+    #   BB
+    #    B Actual Pyramid
+    """
     green_1x4 = kb.Block(length=4, width=1, color="green")
     green_1x4_final_position = Point(x=0.735, y=-0.287, z=0.0)
     blue_1x2 = kb.Block(length=2, width=1, color="blue")
@@ -93,9 +103,69 @@ def main():
 
     fake_action_list = green_1x4_pnp + blue_1x2_pnp + [None]
 
+    green_1x4_desired = Pose2D(x=0.729, y=-0.273, theta=0.0)
+    blue_1x2_desired = Pose2D(x=0.723, y=-0.321, theta=math.pi/2)
+
+    green_1x4_block = kb.Block(length=4, width=1, color="green",
+                               pose=green_1x4_desired)
+    blue_1x2_block = kb.Block(
+        length=2, width=1, color="blue", pose=blue_1x2_desired)
+    """
+
+    # BBB
+    #  B
+    #  B
+    #  B
+    #  B
+    # BBB  Barbell
+    green_1x2 = kb.Block(length=2, width=1, color="green")
+    green_1x2_final_position = Point(x=0.813, y=-0.258, z=0.0)
+
+    blue_1x4 = kb.Block(length=4, width=1, color="blue")
+    blue_1x4_final_position = Point(x=0.738, y=-0.243, z=0.0)
+
+    red_1x2 = kb.Block(length=2, width=1, color="red")
+    red_1x2_final_position = Point(x=0.664, y=-0.227, z=0.0)
+
+    green_1x2_pnp = generate_pick_and_place(
+        green_1x2, green_1x2_final_position, 0, 1)
+
+    blue_1x4_pnp = generate_pick_and_place(
+        blue_1x4, blue_1x4_final_position, 0, 0)
+
+    red_1x2_pnp = generate_pick_and_place(
+        red_1x2, red_1x2_final_position, 0, 1)
+
+    fake_action_list = green_1x2_pnp + red_1x2_pnp + blue_1x4_pnp
+
+    green_1x2_desired = Pose2D(x=0.807, y=-0.245, theta=math.pi/2)
+    blue_1x4_desired = Pose2D(x=0.729, y=-0.241, theta=0)
+    red_1x2_desired = Pose2D(x=0.653, y=-0.232, theta=math.pi/2)
+
+    green_1x2_block = kb.Block(length=4, width=1, color="green",
+                               pose=green_1x2_desired)
+    blue_1x4_block = kb.Block(
+        length=4, width=1, color="blue", pose=blue_1x4_desired)
+    red_1x2_block = kb.Block(
+        length=4, width=1, color="red", pose=red_1x2_desired)
+
+    expectation = kb.EnvState()
+    expectation.add_block(green_1x2_block)
+    expectation.add_block(blue_1x4_block)
+    expectation.add_block(red_1x2_block)
+
+    errortolerancexy = 0.010
+    errortolerancetheta = 0.087
+
+    node_condition1 = iso.categorical_node_match(['length', 'width'],
+                                                 [1, 1])
+    node_condition2 = iso.numerical_edge_match(
+        ['pose_x', 'pose_y'], [0, 0], rtol=errortolerancexy)
+    node_condition3 = iso.numerical_edge_match(
+        ['pose_theta'], [0], rtol=errortolerancetheta)
+
     i = 0
     none_count = 0
-
     while(True):
         action_dict = fake_action_list[i]
 
@@ -117,6 +187,26 @@ def main():
 
         if(none_count == 2):
             rospy.loginfo("Task complete")
+            break
+
+    expectation.print_graph()
+    test_agent.get_ws().print_graph()
+    result1 = nx.is_isomorphic(
+        expectation.ws_state,
+        test_agent.get_ws().ws_state,
+        node_match=node_condition1)
+    result2 = nx.is_isomorphic(
+        expectation.ws_state,
+        test_agent.get_ws().ws_state,
+        node_match=node_condition2)
+    result3 = nx.is_isomorphic(
+        expectation.ws_state,
+        test_agent.get_ws().ws_state,
+        node_match=node_condition3)
+    result = result1 and result2 and result3
+
+    print("Result 1: ", result1, " and Result 2: ", result2,
+          " Result 3: ", result3, " and sum ", result)
 
 
 if __name__ == "__main__":
