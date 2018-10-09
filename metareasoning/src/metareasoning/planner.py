001cadb8cd6c00fb0c3f204ff7f021bb862137e6
zephirefaith <priyam8parashar@gmail.com>
2018-10-09 13:04:30 -0700

    fixed compute_diff() function considers node and edge relations for equivalency

--- metareasoning/src/metareasoning/planner.py
+++ metareasoning/src/metareasoning/planner.py
@@ -3,6 +3,7 @@
 # Simplified planner for creating plan given required change in the workspace
 
 import networkx as nx
+import rospy
 from geometry_msgs.msg import Pose2D
 from metareasoning.knowledge_base import (
     NonPrimitiveActions,
@@ -11,6 +12,7 @@ from metareasoning.knowledge_base import (
     Constraint,
     node2block,
 )
+from metareasoning.utilities import calculate_pose_diff
 
 
 # routine definitions: global so that both planner and learner can access them
@@ -59,6 +61,7 @@ class DeltaPlanner(object):
 
     def __init__(self):
         self._plan_result = False
+        self._plan = None
         self._action_plan = []
         self._state_diff = None
         # FIXME only for A's execution
@@ -69,14 +72,44 @@ class DeltaPlanner(object):
         self._method2action_db[NonPrimitiveActions.deposit] = depositroutine
 
     def _compute_diff(self, curr, reqd):
-        diff_g = nx.difference(reqd.workspace, curr.workspace)
-        self._state_diff = node2block(diff_g[0])
+        rospy.loginfo(list(curr.workspace.nodes(data=True)))
+        rospy.loginfo(list(reqd.workspace.nodes(data=True)))
+        difference = reqd.workspace.copy()
+        # FIXME difference needs to be computed on the type and pose of a block
+        remove_nodes = []
+        for reqd_node, reqd_data in difference.nodes(data=True):
+            for curr_node, curr_data in curr.workspace.nodes(data=True):
+                # get block types from both nodes
+                reqd_block = node2block(difference.nodes[reqd_node])
+                curr_block = node2block(curr.workspace.nodes[curr_node])
+                # get poses from both nodes
+                reqd_pose = Pose2D(reqd_data['pose_x'], reqd_data['pose_y'],
+                                   reqd_data['pose_theta'])
+                curr_pose = Pose2D(curr_data['pose_x'], curr_data['pose_y'],
+                                   curr_data['pose_theta'])
+                pose_diff = calculate_pose_diff(reqd_pose, curr_pose)
+                if reqd_block == curr_block and pose_diff.x < 0.1 and \
+                   pose_diff.y < 0.1 and pose_diff.theta < 0.1:
+                    remove_nodes.append(reqd_node)
+                    break
+        difference.remove_nodes_from(n for n in remove_nodes)
+        node = difference.nodes[list(difference.nodes())[0]]
+        self._state_diff = node2block(node)
 
     def _find_plan(self):
-        # FIXME
-        constraint1 = Constraint()
-        return constraint1
-        pass
+        # use self._state_diff of type Block to get BlockType and BlockPose
+        rospy.loginfo("Finding plan for: %s", str(self._state_diff))
+        block_type = self._state_diff
+        block_pose = self._state_diff.pose
+        constraint1 = Constraint(block=block_type, orientation=0)
+        if abs(abs(self._state_diff.pose.theta) - 1.5) < 0.3:
+            constraint2 = Constraint(position=block_pose, orientation=0)
+        else:
+            constraint2 = Constraint(position=block_pose, orientation=1)
+
+        self._plan = [(NonPrimitiveActions.acquire, constraint1),
+                      (NonPrimitiveActions.deposit, constraint2)]
+        self._plan_result = True
 
     def _plan2action(self, plan):
         action_plan = []
@@ -88,8 +121,7 @@ class DeltaPlanner(object):
         self._compute_diff(curr_state, next_state)
         self._find_plan()
         if self._plan_result:
-            self._plan2action()
-            return self._action_plan
+            return self._plan2action(self._plan)
         else:
             return False
 
